2b5a64fa3e12590fc97d3ce14237b8ca05872612
tuckerbeauchamp <tuckerbeauchamp@gmail.com>
2018-01-10 19:59:35 -0700

    Add react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import {Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Quiz from './components/Quiz';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/quiz">Quiz</Link></li>
+        </ul>
+        <Route exact path="/" component={Home}/>
+        <Route path="/quiz" component={Quiz}/>
       </div>
     );
   }
