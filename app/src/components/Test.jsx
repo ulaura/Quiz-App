8b00cfb56253afe63de8fbce654abe1564821175
tuckerbeauchamp <tuckerbeauchamp@gmail.com>
2018-01-12 01:44:55 -0700

    Add comments for Laura - UofA

--- app/src/components/Test.jsx
+++ app/src/components/Test.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import TestChild from './TestChild';
 
+// This may be a little confusing at first but try and build it out for yourself and it will make some sense
+// you can always slack me and I'll help you out.
+
+
+// Define class component
+// We need React Component Object to add the *React* functionality
 class Test extends React.Component {
+  // The constructor allows us to set our initial state
   constructor(){
     super();
+    // set the state of the component
     this.state = {
       tucker: 0,
       jess: 0
     }
   }
+  // We use the arrow function here so we don't have to worry about having to bind *this* in the constructor
+  // I'm taking a name and id parameter in this function.
+  // So I'm passing this method down into the children components
+  // When the components are clicked on I'll pass the values from inside their scope into here.
   clickHandler = (name, id) => {
     this.setState({
       [name]: id + 1
@@ -19,6 +31,7 @@ class Test extends React.Component {
     console.log(this.state);
     return(
       <div>
+      {/* {...this.state} is a bit of overkill but I wanted to show you that you can use the spread parameter to pass all of state in as props */}
         <TestChild {...this.state} name="tucker" onClick={this.clickHandler} />
         <TestChild {...this.state} name="jess" onClick={this.clickHandler} />
       </div>
--- app/src/components/TestChild.jsx
+++ app/src/components/TestChild.jsx
@@ -4,6 +4,7 @@ const TestChild = (props) => {
   const id = props[props.name];
   return(
     <div>
+    {/* Here we call the method passed down through our props to change our parents state */}
       <h1 onClick={() => props.onClick(props.name, id)}>{props.name} {props[props.name]}</h1>
     </div>
   )
